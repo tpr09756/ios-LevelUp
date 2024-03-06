8a9ac453035d89b12258c2e82fc5f89bf16fad0f
Tiago Rodrigues <tpr09756@gmail.com>
2024-03-06 14:13:09 -0100

    loginView usernameTextField added

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -12,6 +12,8 @@ import UIKit
 // programaticaly create new views:
 class LoginView: UIView {
     
+    let usernameTextField = UITextField()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -34,9 +36,40 @@ extension LoginView {
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameText.delegate = self
     }
     
     func layout() {
+        
+        addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            usernameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: usernameTextField.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
         
     }
 }
---
+++ Bankey/Bankey/Test.swift
@@ -0,0 +1,25 @@
+//
+//  Test.swift
+//  Bankey
+//
+//  Created by Tiago Rodrigues on 06/03/2024.
+//
+
+import XCTest
+ 
+@testable import Bankey
+ 
+class Test: XCTestCase {
+    var vc: ViewController!
+ 
+    override func setUp() {
+        super.setUp()
+        vc = ViewController()
+        vc.loadViewIfNeeded()
+    }
+ 
+    func testShouldBeVisible() throws {
+        let isViewLoaded = vc.isViewLoaded
+        XCTAssertTrue(isViewLoaded)
+    }
+}
