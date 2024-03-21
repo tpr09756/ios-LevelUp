950286c910570b63425c2108d6363346288f26cb
Tiago Rodrigues <tpr09756@gmail.com>
2024-03-21 09:53:29 +0000

    refactor: create generic onboarding view controller
    This commit adds a generic onboarding view controller that can be reused across multiple onboarding screens.

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.backgroundColor = .systemBackground
 //        window?.rootViewController = LoginViewController()
         window?.rootViewController = OnboardingContainerViewController()
+//        window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you fill like you are back in 1989")
         
         return true
     }
