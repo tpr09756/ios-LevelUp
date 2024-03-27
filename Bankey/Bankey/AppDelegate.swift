da7a95cbec7056a6e83d052c418814ebc70a697d
Tiago Rodrigues <tpr09756@gmail.com>
2024-03-27 08:10:47 -0100

    feat: Add logout button

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -26,9 +26,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
         loginViewController.delegate = self
         onboardingViewController.delegate = self
-    
+        
+        registerForNotifications()
+        
         displayLogin()
         return true
+    }
+    
+    private func registerForNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(didLogout()), name: .logout, object: nil)
     }
     
     private func displayLogin() {
@@ -84,7 +90,7 @@ extension AppDelegate: OnboardingContainerViewControllerDelegate {
 }
 
 extension AppDelegate: LogoutDelegate {
-    func didLogout() {
+    @objc func didLogout() {
         setRootViewController(loginViewController)
     }
 }
---
+++ Bankey/Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Tiago Rodrigues on 27/03/2024.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
