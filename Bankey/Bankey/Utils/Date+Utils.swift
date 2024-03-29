6daec2e2635a7c388030078a1192a3eead9953c0
Tiago Rodrigues <tpr09756@gmail.com>
2024-03-29 12:26:46 -0100

    feat: Add networking to AccountSummary

---
+++ Bankey/Bankey/Utils/Date+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Utils.swift
+//  Bankey
+//
+//  Created by Tiago Rodrigues on 28/03/2024.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
